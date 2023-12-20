ac471a1fe2fa8800bb6e0f2698d8d571c10500b8
Kristof Jakab <jaksi@users.noreply.github.com>
2023-12-20 22:11:01 +0000

    DHCP: fixed address for sun

--- nixos/hosts/way.nix
+++ nixos/hosts/way.nix
@@ -48,10 +48,7 @@ in {
         interface = "${lanInterface}";
         dhcp-leasefile = "/nix/persist/dnsmasq.leases";
         dhcp-range = "10.0.0.100,10.0.0.200,12h";
-        dhcp-host = [
-          "ArcherAX55,10.0.0.2"
-          "nas,10.0.0.3"
-        ];
+        dhcp-host = [ "ArcherAX55,10.0.0.2" "nas,10.0.0.3" "sun,10.0.0.4" ];
       };
     };
   };
