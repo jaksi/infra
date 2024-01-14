7ac2b3feadb48d231e38c8fac0b88dd21d06ae41
Kristof Jakab <jaksi@users.noreply.github.com>
2024-01-14 22:33:50 +0000

    add aarch64 hashes for vscode

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -1,7 +1,10 @@
 self: super: {
   vscode-cli = super.callPackage ./vscode.nix {
     package = "cli";
-    hashes.x86_64-linux = "sha256-zEaboEYRQfw6WPWJJixufSv6QwRN0mcmfnlOjYm4MdE=";
+    hashes = {
+      x86_64-linux = "sha256-zEaboEYRQfw6WPWJJixufSv6QwRN0mcmfnlOjYm4MdE=";
+      aarch64-linux = "sha256-Kt49ipcc/mpbQE0yVIR23Cjn6DiljNwW7SePMpcu9Os=";
+    };
     installPhase = ''
       mkdir -p $out/bin
       cp code $out/bin/
@@ -10,7 +13,10 @@ self: super: {
   };
   vscode-server = super.callPackage ./vscode.nix {
     package = "server";
-    hashes.x86_64-linux = "sha256-IFsnV4g7pZJD4WdOVa8PHsRpVq7whdje/H2drK0Xxxg=";
+    hashes = {
+      x86_64-linux = "sha256-IFsnV4g7pZJD4WdOVa8PHsRpVq7whdje/H2drK0Xxxg=";
+      aarch64-linux = "sha256-v1U2NxshWqAro8bskXsNvMMUtFuSbFhFnMgVKDEykN0=";
+    };
     installPhase = ''
       mkdir -p $out
       cp -r vscode-server-*/* $out/
