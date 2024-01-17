3f248847ca7043d07d59873de2a005e83bdee128
Kristof Jakab <jaksi@users.noreply.github.com>
2024-01-17 00:26:51 +0000

    jet: update

--- nixos/hosts/jet.nix
+++ nixos/hosts/jet.nix
@@ -1,16 +1,27 @@
 # Desktop, AMD Zen 4, Nvidia Lovelace
 
-let secrets = import ../secrets.nix;
-in {
-  imports = [ ../hardware/amd.nix ../roles/common.nix ];
+{
+  imports = [ ../hardware/amd.nix ../roles/desktop.nix ../roles/common.nix ];
 
   networking.hostName = "jet";
 
   boot.initrd.availableKernelModules = [ "nvme" ];
-  security.sudo.wheelNeedsPassword = false;
-  users.users.jaksi = {
-    extraGroups = [ "wheel" ];
-    hashedPassword = secrets.hashedUserPassword;
-    isNormalUser = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware = {
+    nvidia = {
+      nvidiaSettings = false;
+      modesetting.enable = true;
+    };
+    opengl.driSupport32Bit = true;
   };
+  programs.sway = {
+    extraOptions = [ "--unsupported-gpu" ];
+    extraSessionCommands = ''
+      export WLR_NO_HARDWARE_CURSORS=1
+    '';
+  };
+  environment.etc."sway/config".text = ''
+    output DP-2 adaptive_sync on
+    output DP-2 mode 2560x1440@279.958Hz
+  '';
 }
